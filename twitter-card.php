ce441d399ce1dcdb7955f841a835c24bce61494d
Chris <chrisdavidramos@gmail.com>
2017-01-14 14:27:20 -0500

    newline after element

--- twitter-card.php
+++ twitter-card.php
@@ -398,7 +398,7 @@ class Twitter_Card {
 			$flag = ENT_XHTML;
 		else if ( defined( 'ENT_HTML5' ) )
 			$flag = ENT_HTML5;
-		return '<meta name="' . self::PREFIX . ':' . htmlspecialchars( $name, $flag ) . '" content="' . htmlspecialchars( $value, $flag ) . '"' . ( $xml === true ? ' />' : '>' );
+		return '<meta name="' . self::PREFIX . ':' . htmlspecialchars( $name, $flag ) . '" content="' . htmlspecialchars( $value, $flag ) . '"' . ( $xml === true ? ' />' : '>' . PHP_EOL);
 	}
 
 	/**
